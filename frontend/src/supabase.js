e2e66d992b5ede5b179a5b79304fd6d75d0e5b85
Michael Kisida <michael@kisida.com>
2023-10-26 22:35:25 -0400

    migrated db

--- frontend/src/supabase.js
+++ frontend/src/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://ryiounkzkfkfllafeogx.supabase.co'
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJ5aW91bmt6a2ZrZmxsYWZlb2d4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTc0ODg5NDgsImV4cCI6MjAxMzA2NDk0OH0.u6-sKAt9ti1EUnJlzB5exbwNw8HyWHKLoTrgSgoHMKI'
+const supabaseUrl = 'https://wwvlemerrmdrbopunhwq.supabase.co'
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Ind3dmxlbWVycm1kcmJvcHVuaHdxIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTgzNzM2MzUsImV4cCI6MjAxMzk0OTYzNX0.E2N2hcBSzSsmJRJcPOiaemgN1MTtrQWSCHqanpxLnSA'
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
